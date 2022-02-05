19572938061de56f44226f4fa862e6e2a5699f37
wGuerrero333 <wguerreroc1969@gmail.com>
2022-02-05 16:42:46 -0500

    Solo el servidor sin html

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
-alert('hey');
 
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome servidor simple.' });
+  });
+  
+
+app.get('/back',(req,res)=>{
+    res.send('Backend ok')
+})
 
 app.listen(port,()=>{
     console.log(`Listened by: ${port} `);
