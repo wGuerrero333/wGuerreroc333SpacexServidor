501885fadca93a85f20c098e0c7e250695d9d3c0
wGuerrero333 <wguerreroc1969@gmail.com>
2022-02-05 16:25:06 -0500

    html solo el express sin rutas

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 alert('hey');
 
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// app.listen(port,()=>{
-//     console.log(`Listened by: ${port} `);
-// })
+app.listen(port,()=>{
+    console.log(`Listened by: ${port} `);
+})
